cf6485d2d3e3536384cf145fcb8e0551fb563e4d
Suman Rasaili <rasailisuman123@gmail.com>
2022-05-09 12:33:52 +0545

    Calculator Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:async';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  //const MyApp({ Key? key }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: "FlutterDemo",
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
@@ -24,72 +25,126 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
+
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late final String _url = " https://owlbot.info/api/v4/dictionary/owl -s | json_pp";
-  late final String  _token = " 476d32ba6375bb7d6d7581bb6efb3b8fda318f13";
-  
- final TextEditingController _controller = TextEditingController();
+  final String _url = "https://owlbot.info/api/v4/dictionary/";
+  final String _token = "c7a0e47311736683d87aa05498b81862dee4f0d4";
+
+  final TextEditingController _controller = TextEditingController();
+
+  late StreamController _streamController;
+  late Stream _stream;
+
+  _search() async {
+    if (_controller.text.isEmpty) {
+      _streamController.add(null);
+      return;
+    }
+
+    _streamController.add("waiting");
+
+    Uri uri = Uri.parse(_url + _controller.text.trim());
+    http.Response response = await http.get(uri,
+        headers: {"Authorization": "Token " + _token});
+    var jsoon = json.decode(response.body);
+    print(jsoon["definitions"]);
+    _streamController.add(json.decode(response.body)["definitions"]);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _streamController = StreamController();
+    _stream = _streamController.stream;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    
     return Scaffold(
-      
       appBar: AppBar(
-        centerTitle: true,
         title: const Text("Dictionary"),
-        
-        bottom:  PreferredSize( 
+        bottom: PreferredSize(
           preferredSize: const Size.fromHeight(48.0),
-        
-          child: Row( 
-            children: <Widget>[ 
+          child: Row(
+            children: <Widget>[
               Expanded(
                 child: Container(
-                  
-                  margin:const  EdgeInsets.only(left:12.0,bottom: 8.0),
-                  decoration: BoxDecoration( 
+                  margin: const EdgeInsets.only(left: 12.0, bottom: 8.0),
+                  decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular( 24.0),
+                    borderRadius: BorderRadius.circular(24.0),
                   ),
-                  child: TextFormField( 
-                    onChanged: (String text){
-
-                    },
+                  child: TextFormField(
+                    onChanged: (String text) {},
                     controller: _controller,
-                    decoration:const  InputDecoration( 
-                      hintText: "Search for a Word",
-                      contentPadding: EdgeInsets.only(left:24.0),
+                    decoration: const InputDecoration(
+                      hintText: "Search for a word",
+                      contentPadding: EdgeInsets.only(left: 24.0),
                       border: InputBorder.none,
-                    
                     ),
                   ),
                 ),
-                
               ),
-              IconButton(onPressed: (){
-
-
-              },
-               icon: const Icon(
-                 Icons.search,
-                 color: Colors.white,
-                 ),),
-              
-            ],  
-                  
+              IconButton(
+                icon: const Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+                onPressed: _search,
+              ),
+            ],
           ),
-          
-        ),  
+        ),
       ),
-      
-      body:Container(
+      body: Container(
+        margin: const EdgeInsets.all(8.0),
+        child: StreamBuilder(
+          stream: _stream,
+          builder: (BuildContext ctx, AsyncSnapshot snapshot){
+            if(snapshot.data == null){
+              return const Center(
+                child: Text("Enter a search word"),
+              );
+            }
 
+            if(snapshot.data == "waiting"){
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
 
+            return ListView.builder(
+               itemCount: snapshot.data.length,
+               itemBuilder: (context, int index){
+               print(snapshot.data.length);
+              return ListBody(
+                children: <Widget> [
+                  Container(
+                    color: Colors.grey[300],
+                    child: ListTile(
+                      leading: snapshot.data[index]["image_url"] == null 
+                      ? null: 
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(snapshot.data[index]["image_url"]),
+                      ),
+                      title: Text(_controller.text.trim() + "(" + snapshot.data[index]["definition"] + ")"),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(snapshot.data[index]["definition"]),
+                  ),
+                ],
+              );
+            },);
+          },
+        ),
       ),
     );
   }
